b77d251e9cf01470365827e2462a1eebb3e78d07
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-11-08 00:13:07 -0500

    implemented useNavigate functionality to toggle between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Leaderboard from "./pages/Leaderboard";
 import "./styles-c/StyledApp.css"
+import AddScoreForm from "./pages/AddScoreForm";
 
 function App() {
   return (
     <div id = "appContainer">
       <Routes>
         <Route path="/" element={<Leaderboard />} />
+        <Route path="/add-score" element={<AddScoreForm />} />
       </Routes>
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,24 @@
+import { NavigateFunction, useNavigate } from "react-router-dom";
 import "../styles-c/StyledButton.css";
+import React from "react";
 
 export default function Button() {
+  const nav: NavigateFunction = useNavigate();
+  const navigateToAddScoreForm = (
+    e: React.MouseEvent<HTMLInputElement>
+  ): void => {
+    e.preventDefault();
+    nav("/add-score");
+  };
   return (
     <div className="button-wrapper">
-      <input type="submit" value="Add a Score" />
+      <input
+        type="submit"
+        value="Add a Score"
+        onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+          navigateToAddScoreForm(e)
+        }
+      />
     </div>
   );
 }
