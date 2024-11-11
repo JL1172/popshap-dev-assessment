26693929fe3d564b112dc33ec4a8c40f4ba61023
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-11-10 22:23:09 -0500

    Update firebaseConfig.ts
    Removed unnecessary comments

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyBwsk77mLpkAnYu3reQQOJbf_vVQhwjNEc",
   authDomain: "developer-assignment-baac6.firebaseapp.com",
@@ -14,7 +12,7 @@ const firebaseConfig = {
   appId: "1:336805703398:web:dd58717dc10e91d9dd5527",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 export { db };
